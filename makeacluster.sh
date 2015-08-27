2e7d8f4dd16882f602fdc415ebfe81e8053d8dae
Vince Gonzalez <vgonzalez@mapr.com>
2015-08-27 12:50:28 -0400

    only use vault password file option if the file exists

--- makeacluster.sh
+++ makeacluster.sh
@@ -1,9 +1,14 @@
 user=ec2-user
 mount=/media/ephemeral0
+vault_password_file="vault_pass"
+
+if test -f $vault_password_file; then
+	VAULT_PASS_OPT="--vault-password-file=$vault_password_file"
+fi
 
 time (
 	if ! test -f "inventory.py" || ! ansible -i inventory.py -u $user -m ping all; then
-		ansible-playbook --vault-password-file=vault_pass mapr_aws_bootstrap.yml
+		ansible-playbook $VAULT_PASS_OPT mapr_aws_bootstrap.yml
 		ansible-playbook -i inventory.py -u $user wait.yml
 
 		# CentOS 6 HVM AMI housekeeping
@@ -21,5 +26,5 @@ time (
 	fi
 
 	ansible-playbook -i inventory.py -u $user wait.yml
-	ansible-playbook -i inventory.py -su $user --vault-password-file=vault_pass mapr_install.yml
+	ansible-playbook -i inventory.py -su $user $VAULT_PASS_OPT mapr_install.yml
 )

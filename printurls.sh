0ea1fd55328853d0261bf23776e543c74cbc097b
Vince Gonzalez <vgonzalez@mapr.com>
2015-12-31 09:47:03 -0500

    adding utility to print out URLs

---
+++ printurls.sh
@@ -0,0 +1,23 @@
+#!/bin/sh 
+
+if jq . < /dev/null ; then
+	# print MCS URLs
+	./inventory.py | jq '.["webserver"] | map("MCS: https://\(.):8443")'
+
+	# print Solr URLs
+	./inventory.py | jq '.["solr"] | map("Solr Admin: http://\(.):8983/solr")'
+
+	# print OpenTSDB URLs
+	./inventory.py | jq '.["opentsdb"] | map("OpenTSDB: http://\(.):4242")'
+
+	# print Hue URLs
+	./inventory.py | jq '.["hue"] | map("Hue: http://\(.):8888")'
+
+	# print Spark URLs
+	# ./inventory.py | jq '.["spark_master"] | map("Spark Master: http://\(.):8080")'
+
+	# print ResourceManager URLs
+	./inventory.py | jq '.["resourcemanager"] | map("Resource Manager: https://\(.):8090")'
+else
+	echo "Install jq to use this utility."
+fi

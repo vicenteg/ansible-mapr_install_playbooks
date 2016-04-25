1a3762822db06f3926fe1dfaf2bd85b0e74c3b2b
Vince Gonzalez <vgonzalez@mapr.com>
2016-04-25 09:56:41 -0400

    more concise

--- printurls.sh
+++ printurls.sh
@@ -7,28 +7,13 @@ fi
 
 if jq . < /dev/null ; then
 	# print edge node IPs
-	./inventory.py | jq '.["edge"] | map("Edge node: \(.)")'
-
-	# print MCS URLs
-	./inventory.py | jq '.["webserver"] | map("MCS: https://\(.):8443")'
-
-	# print Solr URLs
-	./inventory.py | jq '.["solr"] | map("Solr Admin: http://\(.):8983/solr")'
-
-	# print OpenTSDB URLs
-	./inventory.py | jq '.["opentsdb"] | map("OpenTSDB: http://\(.):4242")'
-
-	# print Hue URLs
-	./inventory.py | jq '.["hue"] | map("Hue: http://\(.):8888")'
-
-	# print Spark URLs
-	# ./inventory.py | jq '.["spark_master"] | map("Spark Master: http://\(.):8080")'
-
-	# print Mesos URLs
-	./inventory.py | jq '.["mesosmaster"] | map("Mesos Master: http://\(.):8080")'
-
-	# print ResourceManager URLs
-	./inventory.py | jq '.["resourcemanager"] | map("Resource Manager: https://\(.):8090")'
+	./inventory.py | jq '(.["edge"] | map("Edge node: \(.)")),
+		(.["webserver"] | map("MCS: https://\(.):8443")),
+		(.["solr"] | map("Solr Admin: http://\(.):8983/solr")),
+		(.["opentsdb"] | map("OpenTSDB: http://\(.):4242")),
+		(.["hue"] | map("Hue: http://\(.):8888")),
+		(.["mesosmaster"] | map("Mesos Master: http://\(.):8080")),
+		(.["resourcemanager"] | map("Resource Manager: https://\(.):8090"))'
 else
 	echo "Install jq to use this utility."
 fi

cfd7907891c29f02a23e24069ea13a3dfd80a102
Vince Gonzalez <vgonzalez@mapr.com>
2016-11-03 15:22:18 +0000

    change mesos group names to match ansible-dcos roles

--- printurls.sh
+++ printurls.sh
@@ -13,8 +13,8 @@ if jq . < /dev/null ; then
 		(.["solr"] | map("Solr Admin: http://\(.):8983/solr")),
 		(.["opentsdb"] | map("OpenTSDB: http://\(.):4242")),
 		(.["hue"] | map("Hue: http://\(.):8888")),
-		(.["mesosmaster"] | map("Mesos Masters: http://\(.):5050")),
-		(.["mesosmaster"] | map("Marathon: http://\(.):8080")),
+		(.["mesos-master"] | map("Mesos Masters: http://\(.):5050")),
+		(.["mesos-master"] | map("Marathon: http://\(.):8080")),
 		(.["resourcemanager"] | map("Resource Manager: https://\(.):8090"))'
 else
 	echo "Install jq to use this utility."

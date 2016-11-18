479f263165303e5a78d720d9cc7f80ad13568b60
Vince Gonzalez <vgonzalez@mapr.com>
2016-11-18 19:53:25 +0000

    added --user to set remote username

--- makeacluster.sh
+++ makeacluster.sh
@@ -4,7 +4,7 @@ user=centos
 mount=/media/ephemeral0
 vault_password_file="vault_pass"
 
-OPTS=$(getopt --long awsonly --long validationonly --long skipvalidation --long verbose --long testonly --long skiptests -o "vt" -- "$@")
+OPTS=$(getopt --long awsonly --long validationonly --long skipvalidation --long verbose --long testonly --long skiptests --long user: -o "vtu:" -- "$@")
 if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 
 # Note the quotes around `$TEMP': they are essential!
@@ -14,6 +14,10 @@ SKIP_TAGS=
 
 while true ; do
 	case "$1" in
+		-u|--user)
+			user="$2"
+			echo "remote user = $user"
+			shift ; shift ;;
 		-v|--verbose)
 			VERBOSE="-vv"
 			echo "verbose on." ;
@@ -50,17 +54,14 @@ if test -f $vault_password_file; then
 fi
 
 time (
-	#if ! test -f "inventory.py" || ! ansible $VERBOSE -i inventory.py -u $user -m ping all; then
-	#if ! test -f "inventory.py" ; then
-		ansible-playbook $VERBOSE $VAULT_PASS_OPT mapr_aws_bootstrap.yml || exit 1
-		ansible-playbook $VERBOSE -i inventory.py -u $user wait.yml || exit 1
+	ansible-playbook $VERBOSE $VAULT_PASS_OPT mapr_aws_bootstrap.yml || exit 1
+	ansible-playbook $VERBOSE -i inventory.py -u $user wait.yml || exit 1
 
-		# CentOS 6/7 HVM AMI housekeeping
-		# Wait a bit for cloudinit to finish (presumably)
-		sleep 15
-		ansible $VERBOSE -i inventory.py -su $user -m mount -a "src=/dev/xvdf name=/media/ephemeral0 state=absent fstype=ext3" all
-		ansible-playbook $VERBOSE -i inventory.py -u $user opt_mapr.yml || exit 1
-	#fi
+	# CentOS 6/7 HVM AMI housekeeping
+	# Wait a bit for cloudinit to finish (presumably)
+	sleep 15
+	ansible $VERBOSE -i inventory.py -su $user -m mount -a "src=/dev/xvdf name=/media/ephemeral0 state=absent fstype=ext3" all
+	ansible-playbook $VERBOSE -i inventory.py -u $user opt_mapr.yml || exit 1
 
 	ansible -i inventory.py -m setup --tree facts all
 	if [ -z $AWSONLY ]; then
